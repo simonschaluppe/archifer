0ef597e4a74bee0f55ab8fc6116af2061ee6dda7
simon.schneider@technikum-wien.at <simon.schneider@technikum-wien.at>
2026-05-20 14:30:41 +0200

    asd

--- src/archifer/parser.py
+++ src/archifer/parser.py
@@ -14,4 +14,29 @@ def parse_yaml_config(yaml_path):
 
     return constraints
 
+def apply_constraints(matches, constraints_df,  
+                          archetype_df, 
+                          pl_problem:pl.LpProblem, 
+                          pl_var_dict):
+    """
+    matches: [(constraints_df.column, archetype_column)]
+    """
+    
+    for c in constraints_df.itertuples():
+        geb_eig = c[3]
+        size_class = c[4]
+        age_class = c[5]
+        nfa_target = c[7]
+        selection = archetype_df[
+            (archetype_df["Gebäudeigenschaft (überwiegende Nutzung) (Ebene +2)"] == geb_eig)&
+            (archetype_df["Nettogrundfläche in Quadratmetern (in Klassen) (Ebene +1)"] == size_class)&
+            (archetype_df["Bauperiode (Ebene +2)"] == age_class)
+        ]
+        print(geb_eig, size_class, age_class, nfa_target, selection.index)
+        if len(selection) > 0:
+            pl_problem += (
+                pl.lpSum(pl_var_dict[i+1] for i in selection.index) == nfa_target
+            )
 
+def apply_constraint(selection, lhs_func, rhs_func, pl_problem):
+    pl_problem += pl.lpSum(lhs_func(selection)) 
